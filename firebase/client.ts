d7376abfe90348d8a7733705a479212197e68b0d
laxmibagodi <laxmibagodi25@gmail.com>
2025-06-10 01:43:27 +0530

    updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,35 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { getApp, getApps, initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-// };
-//
-// // Initialize Firebase
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// // const analytics = getAnalytics(app);
-//
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-
-
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -37,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDK3sBcr-TFcxdfEziTA1nEgB3WZaa-O7g",
-  authDomain: "interviewprep-782c5.firebaseapp.com",
-  projectId: "interviewprep-782c5",
-  storageBucket: "interviewprep-782c5.firebasestorage.app",
-  messagingSenderId: "449760286191",
-  appId: "1:449760286191:web:1d01bf6b856475fabe521a",
-  measurementId: "G-QRTGG34YSE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
